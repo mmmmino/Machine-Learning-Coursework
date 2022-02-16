0bca2c975b3ad383f8b3857c973078996a22bec6
mmmmino <91346637+mmmmino@users.noreply.github.com>
2022-02-16 10:58:48 -0600

    hw4

---
+++ hw4-naive-bayes-mmmmino-main/src/utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def softmax(x, axis):
+    """
+    Implements a *stabilized* softmax along the correct index
+    https://www.deeplearningbook.org/contents/numerical.html
+
+    Do not use scipy to implement this function!
+    """
+    x = np.atleast_2d(x)
+    softmax_ = np.zeros(np.shape(x))
+    max_ = np.max(x, axis=axis)
+    for i in range(0, x.shape[0]):
+        numerator = np.exp(x[i] - max_[i], dtype=np.float32)
+        softmax_[i] = numerator / np.sum(numerator)
+    return softmax_
+
